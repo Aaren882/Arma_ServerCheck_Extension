2c4359860d4376a3f7422fc512cf81def50f73d7
Aaren <aaren.yang@gmail.com>
2025-01-30 16:15:51 +0800

    Update Packages - Fix Compat

--- Arma_ServerCheck_Extension/DllEntry.cs
+++ Arma_ServerCheck_Extension/DllEntry.cs
@@ -1,10 +1,10 @@
-﻿using RGiesecke.DllExport;
-using SteamServerQuery;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using RGiesecke.DllExport;
+using SteamServerQuery;
 
 namespace Arma_ServerCheck_Extension
 {
@@ -48,15 +48,16 @@ namespace Arma_ServerCheck_Extension
 			//- Check Server Status
 			try
 			{
-				var server = GetSteamServer(function, args[0]);
-				if (server == "")
-				{
-					output.Append("No function be found");
-					return -1;
-				}
+                var server = GetSteamServer(function, args[0]);
 
-				//- Setup Serialization
-				string jsonString = JsonSerializer.Serialize(server[0],
+                if (server.ToString() == "")
+                {
+                    output.Append("Request Timeout.");
+                    return -1;
+                }
+
+                //- Setup Serialization
+                string jsonString = JsonSerializer.Serialize(server,
 					new JsonSerializerOptions
 					{
 						Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping, //- Output all Text
@@ -65,29 +66,43 @@ namespace Arma_ServerCheck_Extension
 				);
 				int[] UTF = Tools.StringToCode32(jsonString);
 
-				//- Return Values (in 32Bit Unicode)
-				output.Append($"[{string.Join(",", UTF)}]");
+                //- Return Values (in 32Bit Unicode)
+                output.Append($"[{string.Join(",", UTF)}]");
 				return 0;
 			}
 			catch (Exception e)
 			{
-				Tools.Logger(e);
+				Tools.Logger(e, e.Message);
 				return -1;
 			}
 		}
 
-		private static dynamic GetSteamServer(string function, string input)
-		{
-			string[] info = input.Trim('\"').Split(':');
-			Tools.Logger(null, info[1]);
-			Tools.Logger(null, $"{Int16.Parse(info[1])}");
+        private static dynamic GetSteamServer(string function, string input)
+        {
+            string[] info = input.Trim('\"').Split(':');
 
-            if (function == "ServerAsync") //- [IP, Steam Port, Timeout(ms)];
-				return SteamServer.QueryServerAsync(info[0], Int16.Parse(info[1]) + 1, 3000).Result;
-			if (function == "PlayersAsync") //- [IP, Steam Port, Timeout(ms)];
-				return SteamServer.QueryPlayersAsync(info[0], Int16.Parse(info[1]) + 1, 3000).Result;
-			
-			return "";
-		}
-	}
+            try
+            {
+                dynamic result = "";
+                if (function == "ServerAsync") //- [IP, Steam Port, Timeout(ms)];
+                    result = SteamServer.QueryServerAsync(info[0], Int16.Parse(info[1]) + 1, 3000).Result;
+                if (function == "PlayersAsync") //- [IP, Steam Port, Timeout(ms)];
+                    result = SteamServer.QueryPlayersAsync(info[0], Int16.Parse(info[1]) + 1, 3000).Result;
+                return result;
+            }
+            catch (AggregateException e)
+            {
+				//- Check if Exception is Timeout
+                foreach (var item in e.InnerExceptions)
+                {
+                    if (item.GetType() == new SteamException().GetType())
+                    {
+                        return "";
+                    }
+                }
+				Tools.Logger(e, e.Message);
+                return "";
+            }
+        }
+    }
 }
